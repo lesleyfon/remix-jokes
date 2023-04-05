d0e59f49ea3b8afe8e06119f39cd0091f8f54e9f
lesley <lesleybanadzem2017@gmail.com>
2023-04-04 21:54:01 -0500

    Added logic for getting sing joke

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,8 +1,38 @@
+import { Joke } from "@prisma/client";
+import { LoaderArgs } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+import invariant from "tiny-invariant";
+import { Link, useLoaderData } from "@remix-run/react";
+
+type LoaderData = { joke: Joke | null };
+export const loader = async ({ params }: LoaderArgs) => {
+	const { jokeId } = params;
+
+	invariant(jokeId, `params.slug is required`);
+
+	const data: LoaderData = {
+		joke: await db.joke.findUnique({
+			where: {
+				id: jokeId,
+			},
+		}),
+	};
+	if (!data.joke) {
+		throw new Error("Joke not found");
+	}
+
+	return data;
+};
+
 export default function JokeRoute() {
+	const { joke } = useLoaderData<typeof loader>();
+
+	invariant(joke, "Error, no Joke");
+
 	return (
 		<div>
-			<p>Here's your hilarious joke:</p>
-			<p>Why don't you find hippopotamuses hiding in trees? They're really good at it.</p>
+			<p>{joke.content}</p>
+			<Link to=".">{joke.name} Permalink</Link>
 		</div>
 	);
 }
