2b27ce3795c34b5f55b1ff124001a201573783d5
lesley <lesleybanadzem2017@gmail.com>
2023-04-04 22:05:21 -0500

    Added functionality for getting random jokes

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,26 @@
+import { db } from "~/utils/db.server";
+import { Joke } from "@prisma/client";
+import invariant from "tiny-invariant";
+import { Link, useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+	const count = await db.joke.count();
+
+	const randomRowNumber = Math.floor(Math.random() * count);
+	const randomJoke = await db.joke.findMany({
+		take: 1,
+		skip: randomRowNumber,
+	});
+
+	return { joke: randomJoke[0] };
+};
 export default function JokesIndexRoute() {
+	const { joke } = useLoaderData<typeof loader>();
 	return (
 		<div>
 			<p>Here's a random joke:</p>
-			<p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+			<p>{joke.content}</p>
+			<Link to=".">{joke.name} Permalink</Link>
 		</div>
 	);
 }
