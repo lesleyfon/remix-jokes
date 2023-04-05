33e74fe180624e9f2b109f86fe84bb5572c804d4
lesley <lesleybanadzem2017@gmail.com>
2023-04-04 23:16:31 -0500

    Added functionality for login in

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -4,6 +4,7 @@ import { Link, useActionData, useSearchParams } from "@remix-run/react";
 import stylesUrl from "~/styles/login.css";
 import { db } from "~/utils/db.server";
 import { badRequest } from "~/utils/request.server";
+import { createUserSession, login } from "~/utils/session.server";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: stylesUrl }];
 
@@ -66,11 +67,17 @@ export const action = async ({ request }: ActionArgs) => {
 			// login to get the user
 			// if there's no user, return the fields and a formError
 			// if there is a user, create their session and redirect to /jokes
-			return badRequest({
-				fieldErrors: null,
-				fields,
-				formError: "Not implemented",
-			});
+			const user = await login({ username, password });
+
+			if (!user) {
+				return badRequest({
+					fields,
+					fieldErrors: null,
+					formError: "Incorrect username and password",
+				});
+			}
+
+			return await createUserSession(user.id, redirectTo);
 		}
 		case "register": {
 			const userExists = await db.user.findFirst({
