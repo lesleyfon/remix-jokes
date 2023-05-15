356accc6413e62baa2fe0533a822c0de5bc30a45
lesley <lesleybanadzem2017@gmail.com>
2023-05-14 21:31:03 -0500

    Implimented functionality for pulling jokes by a user when they are logged in. And displaying user information

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -2,28 +2,38 @@ import { Outlet, Link, useLoaderData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
 import { json, LinksFunction, LoaderFunction } from "@remix-run/node";
 import stylesUrl from "~/styles/jokes.css";
-import { Joke } from "@prisma/client";
+import type { Joke, User } from "@prisma/client";
+import { getUser } from "~/utils/session.server";
 
 type LoaderData = {
 	jokes: Array<Pick<Joke, "id" | "name">>;
+	jokesListItems?: Array<Pick<Joke, "id" | "name">>;
+	user: User | null;
 };
 
 export const links: LinksFunction = () => {
 	return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export const loader: LoaderFunction = async () => {
+type LoaderType = {
+	request: Request;
+};
+
+export const loader: LoaderFunction = async ({ request }: LoaderType) => {
+	const user = await getUser(request);
+
 	const jokes = await db.joke.findMany({
 		take: 5,
 		select: { id: true, name: true },
 		orderBy: { createdAt: "desc" },
 	});
 
-	return { jokes };
+	const data: LoaderData = { jokes, user };
+	return data;
 };
 
 export default function JokesRoute() {
-	const { jokes } = useLoaderData<LoaderData>();
+	const { jokes, user } = useLoaderData<LoaderData>();
 
 	return (
 		<div className="jokes-layout">
@@ -35,6 +45,18 @@ export default function JokesRoute() {
 							<span className="logo-medium">J🤪KES</span>
 						</Link>
 					</h1>
+					{user ? (
+						<div className="user-info">
+							<span>{`Hi ${user.username}`}</span>
+							<form action="/logout" method="post">
+								<button type="submit" className="button">
+									Logout
+								</button>
+							</form>
+						</div>
+					) : (
+						<Link to="/login">Login</Link>
+					)}
 				</div>
 			</header>
 			<main className="jokes-main">
